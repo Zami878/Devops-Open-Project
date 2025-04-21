636550cf863c1a83666928d42969363d9a239ad6
BharadwajShivanya <144115901+BharadwajShivanya@users.noreply.github.com>
2025-04-21 18:59:02 +0530

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -429,3 +429,19 @@ function rotationZ(angle) {
         [0,0,0,1]
     ];
 }
+
+function multiplyMatrices(m1, m2) {  // takes the input of two matrix 
+    const result = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]; // enmpty result value matrix
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let sum = 0;
+            for (let k = 0; k < 4; k++) {
+                sum += m1[i][k] * m2[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
+} 
+
