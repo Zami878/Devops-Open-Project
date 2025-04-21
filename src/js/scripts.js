8b65e6963972457407c39f3edfce17b09d308fae
BharadwajShivanya <144115901+BharadwajShivanya@users.noreply.github.com>
2025-04-21 19:09:26 +0530

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -473,3 +473,15 @@ function perspectiveMatrix(fov, aspect, near, far) {
         [0, 0, -1, 0] // Projects Z onto W
     ];
 }
+
+function multiplyMatrixVector(matrix, vector) {
+    const [x, y, z, w = 1] = vector;
+    const m = matrix;
+
+    return [
+        m[0][0]*x + m[0][1]*y + m[0][2]*z + m[0][3]*w,
+        m[1][0]*x + m[1][1]*y + m[1][2]*z + m[1][3]*w,
+        m[2][0]*x + m[2][1]*y + m[2][2]*z + m[2][3]*w,
+        m[3][0]*x + m[3][1]*y + m[3][2]*z + m[3][3]*w
+    ];
+} 
